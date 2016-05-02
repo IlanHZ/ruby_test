615e6ebb2a48c313be7471a8d933819dea11d920
Ilan Hazan <ilaneliehazan@gmail.com>
2016-05-02 13:04:12 +0100

    views

--- app.rb
+++ app.rb
@@ -11,3 +11,57 @@ get "/about" do
   erb :about
 end
 
+# RESTful routes
+
+#INDEX
+
+get "/books" do
+
+end
+
+
+#NEW
+get "/books/new" do
+
+end
+
+#CREATE
+post "/books" do
+
+end
+
+#SHOW
+get "/books/:id" do
+
+end
+
+#EDIT
+get "/books/:id/edit" do
+
+end
+
+#UPDATE (PUT or PATCH)
+# Updates a specific book
+put "/books/:id" do
+
+end
+
+# Partially updates a specific book
+patch "/books/:id" do
+
+end
+
+#DELETE
+delete "books/:id" do
+
+end
+
+
+
+
+
+
+
+
+
+
